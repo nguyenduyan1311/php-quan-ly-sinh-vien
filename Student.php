e19258d86eed3a98ede9d1fd4027999367e1acc6
nguyenduyan1311 <annguyenazn1311@gmail.com>
2021-01-05 08:40:06 +0700

    Student Management

---
+++ Student.php
@@ -0,0 +1,173 @@
+<?php
+
+
+class Student
+{
+    private $id;
+    private $code;
+    private $name;
+    private $gender;
+    private $class;
+    private $birth;
+    private $address;
+    private $phone;
+    private $identity;
+
+    public function __construct($id, $code, $name, $gender, $class, $birth, $address, $phone, $identity)
+    {
+        $this->id = $id;
+        $this->code = $code;
+        $this->name = $name;
+        $this->gender = $gender;
+        $this->class = $class;
+        $this->birth = $birth;
+        $this->address = $address;
+        $this->phone = $phone;
+        $this->identity = $identity;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * @param mixed $code
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getGender()
+    {
+        return $this->gender;
+    }
+
+    /**
+     * @param mixed $gender
+     */
+    public function setGender($gender)
+    {
+        $this->gender = $gender;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getClass()
+    {
+        return $this->class;
+    }
+
+    /**
+     * @param mixed $class
+     */
+    public function setClass($class)
+    {
+        $this->class = $class;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBirth()
+    {
+        return $this->birth;
+    }
+
+    /**
+     * @param mixed $birth
+     */
+    public function setBirth($birth)
+    {
+        $this->birth = $birth;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * @param mixed $address
+     */
+    public function setAddress($address)
+    {
+        $this->address = $address;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+
+    /**
+     * @param mixed $phone
+     */
+    public function setPhone($phone)
+    {
+        $this->phone = $phone;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIdentity()
+    {
+        return $this->identity;
+    }
+
+    /**
+     * @param mixed $identity
+     */
+    public function setIdentity($identity)
+    {
+        $this->identity = $identity;
+    }
+}
